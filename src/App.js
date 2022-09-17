13ad95f18e0b07862b1f587154b7536cd5dedccb
little-sealion <tess.zheng@outlook.com>
2022-09-17 15:14:06 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [name, setName] = useState('');
+  // useEffect(() => {
+  //   console.log('fire every time the component renders');
+  // });
+
+  // useEffect(() => {
+  //   console.log('only fire when the component first mounted');
+  // }, []);
+
+  useEffect(() => {
+    console.log('fire when first mounted and every time when width changes');
+  }, [width]);
+
+  const updateWindowWidth = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Windows width:{width}</h1>
+      Name:
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 }
